014cd1f9da89f48e88e95fae96ba7633ce04b13c
ivan <royal_88@mail.ru>
2024-07-18 19:38:05 +0300

    make the default font foreground white

--- custom.el
+++ custom.el
@@ -9,4 +9,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "gray27" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 128 :width normal :foundry "DAMA" :family "Ubuntu Mono")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "gray27" :foreground "white smoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 151 :width normal :foundry "DAMA" :family "Ubuntu Mono")))))
